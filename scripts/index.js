c9d45b5cc1d9a4cf1d4b17be357dfa46385f81dc
juneate <roccopan@gmail.com>
2019-04-10 20:38:09 -0400

    Menu collapse

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+// Select all the items I want to interact with
+var elMainMenu = document.getElementById('mainmenu');
+var elHambBtn = document.querySelector('.hamburger');
+
+
+// If that test is successful, assign a "listener" to the hamburger button
+// Put a small test output in there: console.log('click!')
+elHambBtn.addEventListener('click', function(e) {
+  elMainMenu.classList.toggle('close');
+});
